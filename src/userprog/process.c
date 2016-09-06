e73736bf5b7b594d34b1b9df9dacbb488947d7f4
Mehul Jain <mehul.jain2029@gmail.com>
2016-09-06 11:47:43 +0530

    minor

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -65,7 +65,7 @@ start_process (void *file_name_)
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp, &start_ptr);
+  success = load (file_name, &if_.eip, &if_.esp, &save_ptr);
 
   /* If load failed, quit. */
   palloc_free_page (file_name);
@@ -446,7 +446,7 @@ setup_stack (void **esp, const char *file_name, char** save_ptr)
       if (success)
         {
           *esp = PHYS_BASE;
-          char *token;
+          char *token, DEFAULT_ARGV = 2, WORD_SIZE = 4;
           char **argv = malloc(DEFAULT_ARGV*sizeof(char *));
           int i, argc = 0, argv_size = DEFAULT_ARGV;
 
