8415fdfd15020326c647b62fdcb661694e377bda
Abheek Ghosh <abheekghosh.ag@gmail.com>
2016-09-11 11:40:04 +0530

    implement sys calls: halt, exec, wait, create, remove, open

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,33 @@
 
 static void syscall_handler (struct intr_frame *);
 
+void halt (void) ;
+void exit (int status) ;
+pid_t exec (const char *cmd_line) ;
+int wait (pid_t pid) ;
+bool create (const char *file, unsigned initial_size) ;
+bool remove (const char *file) ;
+int open (const char *file) ;
+int filesize (int fd) ;
+int read (int fd, void *buffer, unsigned size) ;
+int write (int fd, const void *buffer, unsigned size) ;
+void seek (int fd, unsigned position) ;
+unsigned tell (int fd) ;
+void close (int fd) ;
+
+// File lock
+static struct lock fileLock;
+
+struct userFile_t {
+	fid_t fid;
+	struct list_elem threadElement;
+	struct file *f;
+};
+
+static struct userFile_t *fileFromFid (fid_t);
+static fid_t allocateFid (void);
+static mapid_t allocate_mapid (void);
+
 void
 syscall_init (void) 
 {
@@ -17,4 +44,120 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+void
+halt (void) {
+	shutdown_power_off ();
+}
+
+void
+exit (int status) {
+
+}
+
+pid_t
+exec (const char *cmd_line) {
+	lock_acquire (&fileLock);
+	int returnValue = process_execute (cmd_line);
+	lock_release (&fileLock);
+	return returnValue;
+}
+
+void
+wait (pid_t pid) {
+	process_wait (pid);
+}
+
+bool
+create (const char *file, unsigned initial_size) {
+	if (file == NULL) 
+		exit (-1);
+	
+	lock_acquire (&fileLock);
+	int returnValue = filesys_create (file, initial_size);
+	lock_release (&fileLock);
+	return returnValue;
+}
+
+bool
+remove (const char *file, unsigned initial_size) {
+	if (file == NULL) 
+		exit (-1);
+	
+	lock_acquire (&fileLock);
+	int returnValue = filesys_remove (file, initial_size);
+	lock_release (&fileLock);
+	return returnValue;
+}
+
+int
+open (const char *file) {
+	if (file == NULL) 
+		return -1;
+	
+	struct file *openFile;
+	struce userFile_t *userFile;
+
+	lock_acquire (&fileLock);
+	openFile = filesys_open (file);
+	lock_release (&fileLock);
+
+	if (openFile == NULL) 
+		return -1;
+
+	userFile = (struct userFile_t *) malloc (sizeof (struct userFile_t));
+	if (userFile == NULL) {
+		file_close (openFile);
+		return -1;
+	}
+
+	lock_acquire (&fileLock);
+	list_push_back (&thread_current ()->files, &userFile->threadElement);
+	userFile->fid = allocate_fid ();
+	userFile->f = openFile;
+	lock_release (&fileLock);
+
+	return userFile->fid;	
+}
+
+int filesize (int fd) ;
+int read (int fd, void *buffer, unsigned size) ;
+int write (int fd, const void *buffer, unsigned size) ;
+void seek (int fd, unsigned position) ;
+unsigned tell (int fd) ;
+void close (int fd) ;
+
+// fid allocation
+static fid_t
+allocate_fid (void)
+{
+  static fid_t nextFid = 2;
+  return nextFid++;
+}
+
+// mapid allocation
+static mapid_t
+allocate_mapid (void)
+{
+  static mapid_t nextMapid = 0;
+  return nextMapid++;
+}
+
+static struct userFile_t *
+fileFromFid (int fid)
+{
+	struct thread *th;
+	struct list_elem *it;
+
+	th = thread_current();
+	for (it = list_begin (&th->files); it != list_end (&th->files);
+		it = list_next (it))
+	{
+		struct userFile_t *userFile = list_entry (it, struct userFile_t, thread_elem);
+		if (userFile->fid == fid)
+			return userFile;
+	}
+
+	return NULL;
 }
